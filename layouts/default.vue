0d9fc7533537ea58e19daa91d921e5cb7dc56699
jonathan_ellos <jonathan.marquez@ellos.se>
2022-06-29 00:19:08 +0200

    fixed ui import

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="section is-large">
-    <img src="logo.png" alt="Unai Logo">
+    <the-nav :links="links"></the-nav>
     <slot />
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TheNav from "unai-ui/components/patterns/TheNav";
+const links = [
+  { title: "Home", link: "/" },
+  { title: "Logo", link: "/", type: "img", src: "/logo.png" },
+  { title: "Contact", link: "/contact" },
+];
+</script>
 <style>
 body {
   background-color: rgba(0, 30, 38);
